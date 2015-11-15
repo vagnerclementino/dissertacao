8af36e4cfe3cec5ca043d3ec7c1fad277c92cd7a
Vagner <vagner.clementino@gmail.com>
2015-11-15 19:32:58 -0200

    Inclusão do arquivo antes da revisão geral e ortográfica

---
+++ auto/Proposta_Dissertacao_Vagner_Clementino.el
@@ -0,0 +1,44 @@
+(TeX-add-style-hook
+ "Proposta_Dissertacao_Vagner_Clementino"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("ppgccufmg" "msc" "proposal" "hidelot" "hideabstract")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "brazil") ("fontenc" "T1") ("inputenc" "utf8x") ("hyperref" "a4paper" "portuguese" "bookmarks=true" "bookmarksnumbered=true" "linktocpage" "colorlinks=true" "citecolor=black" "urlcolor=black" "linkcolor=black" "filecolor=black" "") ("xcolor" "table" "xcdraw")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "ppgccufmg"
+    "ppgccufmg10"
+    "babel"
+    "fontenc"
+    "inputenc"
+    "graphicx"
+    "hyperref"
+    "xcolor")
+   (LaTeX-add-labels
+    "ch:Contexto"
+    "fig:world_map"
+    "ch:justificativa"
+    "fig:fluxo_dados"
+    "fig:interfaces"
+    "fig:fluxo_info_xbrl"
+    "fig:docs_xbrl"
+    "fig:taxonomia_xbrl"
+    "fig:arquivo_xsd"
+    "ch:revisao"
+    "ch:metodologia"
+    "sec:revisao_sistematica"
+    "sec:survey"
+    "sec:prova-conceito"
+    "sec:avaliacao"
+    "ch:conclusao_trab_futuros"
+    "tab:cronograma")))
+
---
+++ bib/auto/bibliografia.el
@@ -0,0 +1,27 @@
+(TeX-add-style-hook
+ "bibliografia"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Nakagawa:2013:SAF:2489850.2489853"
+    "RFC4627"
+    "Andrikopoulos:2013:TEO:2491845.2491890"
+    "gmlpaper"
+    "smith1988sgml"
+    "Fawcett:2012:BX:2408362"
+    "bergeron2004essentials"
+    "hoffman2001xbrl"
+    "peixoto2008comparison"
+    "journals/ijcsa/Pierre08"
+    "lupacsc2010role"
+    "mccarthy1982rea"
+    "gailly2007positioning"
+    "sugumaran2002ontologies"
+    "Bosch:2013:APD:2575980.2575988"
+    "wohlin2012experimentation"
+    "baeza1999modern"
+    "booch2000uml"
+    "keele2007guidelines"
+    "nt_03_2013"
+    "fink2003survey"
+    "kitchenham2009systematic")))
+
