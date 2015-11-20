73d7eb29c1c14aea936cc606e97b810deba31ab3
Vagner <vagner.clementino@gmail.com>
2015-11-19 23:05:55 -0200

    Mudança para nova versão da proposta

--- docs/proposta/bib/auto/bibliografia.el
+++ docs/proposta/bib/auto/bibliografia.el
@@ -2,27 +2,9 @@
  "bibliografia"
  (lambda ()
    (LaTeX-add-bibitems
-    "Nakagawa:2013:SAF:2489850.2489853"
-    "RFC4627"
-    "Andrikopoulos:2013:TEO:2491845.2491890"
-    "gmlpaper"
-    "smith1988sgml"
-    "Fawcett:2012:BX:2408362"
-    "bergeron2004essentials"
-    "hoffman2001xbrl"
-    "peixoto2008comparison"
-    "journals/ijcsa/Pierre08"
-    "lupacsc2010role"
-    "mccarthy1982rea"
-    "gailly2007positioning"
-    "sugumaran2002ontologies"
-    "Bosch:2013:APD:2575980.2575988"
-    "wohlin2012experimentation"
-    "baeza1999modern"
-    "booch2000uml"
+    "5741246"
     "keele2007guidelines"
-    "nt_03_2013"
-    "fink2003survey"
-    "kitchenham2009systematic"
-    "lymer1999business")))
+    "wohlin2012experimentation"
+    "101186"
+    "baeza1999modern")))
 
