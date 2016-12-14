223dd369e4a224ba5bcfa9b5a085cf47b5454de5
VAGNER CLEMENTINO DOS SANTOS <pr003252@pbh.rmi>
2016-12-14 08:17:28 -0200

    Renomeando arquivos

--- 90-dissertacao-data/Scripts/ddl/tables/public_traducao_termos.sql
+++ 90-dissertacao-data/Scripts/ddl/tables/public_traducao_termos.sql
@@ -3,7 +3,7 @@ create table public.traducao_termos(
 	id_traducao_termos SERIAL not null,
 	pt_br_termo TEXT not null,
 	en_us_termo TEXT not null,
-	data_atualizada timestamp default now()
+	data_atualizacao timestamp default now()
 	);
 
 alter table public.traducao_termos
