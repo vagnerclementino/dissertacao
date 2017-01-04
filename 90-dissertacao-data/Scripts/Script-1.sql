8315775f6994405ca810a7acc4d266472dfd38bf
Vagner Clementino <vagner.clementino@gmail.com>
2017-01-04 00:28:24 -0200

    Redigindo capítulo de mapeamento sistemático

--- 90-dissertacao-data/Scripts/Script-1.sql
+++ 90-dissertacao-data/Scripts/Script-1.sql
@@ -1,7 +1,2 @@
-select t.dimensao_melhoria, 
-	   t.topico_classificacao,
-	   count(1)
+select *
 from mapeamento_its.temp_esquema_classificao_melhoria t
-group by t.dimensao_melhoria, 
-	   t.topico_classificacao
-order by t.dimensao_melhoria, count(1) desc
