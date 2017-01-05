ea32456daae649408b3ed3ef9acd0ec64ecaa603
Vagner Clementino <vagner.clementino@gmail.com>
2017-01-05 16:34:13 -0200

    Escrvendo capítulo sobre mapeamento

--- 90-dissertacao-data/Scripts/Script-1.sql
+++ 90-dissertacao-data/Scripts/Script-1.sql
@@ -1,4 +1,5 @@
 select *
 from mapeamento_its.temp_esquema_classificao_melhoria t
-where t.dimensao_melhoria = 'Informação'
+where t.dimensao_melhoria = 'Processo'
+and t.termo_dimensao = 'duplicate bug reports'
 order by dimensao_melhoria, topico_classificacao
