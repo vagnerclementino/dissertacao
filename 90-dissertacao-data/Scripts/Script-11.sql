b01f9afea44bdac848ad7edb73dbe182dea587d8
Vagner Clementino <vagner.clementino@gmail.com>
2017-03-02 10:52:03 -0300

    Versão para revisaõ amor.

--- 90-dissertacao-data/Scripts/Script-11.sql
+++ 90-dissertacao-data/Scripts/Script-11.sql
@@ -1,4 +1,25 @@
-SELECT pratica_adotaca as pratica_adotada,
-	   count(1) as total
-FROM "temp-adocao-praticas-ageis"
-group by  pratica_adotaca
+select
+	t.id_temp_cartoes_ordenados,
+	nome_ferramenta,
+	nome_funcionalidade,
+	descricao_funcionalidade,
+	topico_funcionalidade,
+	categoria,
+	dimensao
+from
+	caracterizacao_its.temp_cartoes_ordenados t
+where t.dimensao like 'Processo%'
+order by t.dimensao, t.categoria, t.topico_funcionalidade;
+
+
+select
+
+	t.categoria,
+	count(1)
+from
+	caracterizacao_its.temp_cartoes_ordenados t
+where t.categoria is not null
+group by t.categoria
+order by count(1) desc;
+
+
