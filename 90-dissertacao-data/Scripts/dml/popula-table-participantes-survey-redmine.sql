f32b9a11f51e380c545a5fc3f77a10e8d4d1383e
Vagner Clementino <vagner.clementino@gmail.com>
2017-04-29 19:40:18 -0300

    Alterada as sugestões de melhorias.

---
+++ 90-dissertacao-data/Scripts/dml/popula-table-participantes-survey-redmine.sql
@@ -0,0 +1,31 @@
+insert
+	into
+		email_sender.participantes_survey(
+			id_participantes_survey,
+			nome_participante,
+			email_participante,
+			data_atualizacao,
+			id_grupo_participante,
+			ind_ativo
+		)(
+			select
+				nextval(
+					'email_sender.participantes_survey_id_participantes_survey_seq'::regclass
+				) as id_participantes_survey,
+				cp.nome_contribuidor as nome_participante,
+				cp.email_contribuidor as email_participante,
+				now() as data_atualizacao,
+				(
+					select
+						gp.id_grupo_participante
+					from
+						email_sender.grupo_participante gp
+					where
+						gp.nome_grupo = 'REDMINE'
+				) as id_grupo_participante,
+				'N'::bpchar as ind_ativo
+			from
+				public.contribuidores_projetos cp
+			where
+				cp.projeto = 'redmine/redmine'
+		);
